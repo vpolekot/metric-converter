22ec9ecb5d4dff572b32755bb1b46766f38213b0
Diana Bukhtoiarova <Diana_Bukhtoiarova@epam.com>
2022-09-10 16:30:37 +0300

    added comments

--- main.go
+++ main.go
@@ -1,3 +1,6 @@
+// Package metricConverter is a converter to-from metric system.
+//
+// Lightweight implementation.
 package metricConverter
 
 import "fmt"
@@ -6,14 +9,17 @@ const CentimetersPerInch = 2.54
 const MetersPerFoot = 0.3048
 const LitersPerPint = 0.4732
 
+// Shows converted inches to centimeter without rounding.
 func GetInchToCentimeter(inches float64) {
 	fmt.Printf("%v in. = %v cm \n", inches, inches*CentimetersPerInch)
 }
 
+// Shows converted liters to pints without rounding.
 func GetLiterToPint(liters float64) {
 	fmt.Printf("%v liters = %v pints \n", liters, liters/LitersPerPint)
 }
 
+// Shows converted feets to meter without rounding.
 func GetFootToMeter(feets float64) {
 	fmt.Printf("%v ft. = %v m \n", feets, feets*MetersPerFoot)
 }
