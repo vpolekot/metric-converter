8875ec2234e26efdeda7e5981e5e7f33cf1f2293
Diana Bukhtoiarova <Diana_Bukhtoiarova@epam.com>
2022-09-10 16:02:51 +0300

    fixed functions visibility

--- main.go
+++ main.go
@@ -6,14 +6,14 @@ const CentimetersPerInch = 2.54
 const MetersPerFoot = 0.3048
 const LitersPerPint = 0.4732
 
-func getInchToCentimeter(inches float64) {
+func GetInchToCentimeter(inches float64) {
 	fmt.Printf("%v in. = %v cm", inches, inches*CentimetersPerInch)
 }
 
-func getLiterToPint(liters float64) {
+func GetLiterToPint(liters float64) {
 	fmt.Printf("%v liters = %v pints", liters, liters/LitersPerPint)
 }
 
-func getFootToMeter(feets float64) {
+func GetFootToMeter(feets float64) {
 	fmt.Printf("%v ft. = %v m", feets, feets*MetersPerFoot)
 }
